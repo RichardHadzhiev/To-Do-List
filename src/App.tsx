9a74e3f7b019de1db3e8cdd394c6f2822c3b79ef
Richard Hadzhiev <ri4koto100@gmail.com>
2024-07-01 23:57:30 +0200

    add 3 sections + styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div>To do list</div>;
+  return (
+    <div className="app">
+      <header className="header">Header</header>
+      <main className="main">
+        <section className="tasks">Sec1</section>
+        <section className="tasks">sec2</section>
+        <section className="tasks">sec3</section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
